4bdc78e186b5725426f7cc5e4656799c38efde5d
Muhammad Kashif <mk.kashif.se@gmail.com>
2022-06-10 22:19:54 +0500

    code clean up

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -8,12 +8,7 @@ const {
   promises: { readdir, readFile },
 } = require("fs");
 
-const getDirectories = async (source) =>
-  (await readdir(source, { withFileTypes: true }))
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-
-const getAllContractFiles = async () => {
+const compileAndDeployAllContracts = async () => {
   try {
     const contractsFileNames = (await readdir("./../contracts")).map(
       (item) => item.split(".sol")[0]
@@ -41,43 +36,6 @@ const getAllContractFiles = async () => {
   } catch (error) {
     console.error(error);
   }
-  // const files = (
-  //   await Promise.all(
-  //     contractsFolder.map((item) => {
-  //       const fileName = item.split("sol");
-  //       return readFile(path + "" + item + "/" + fileName[0] + "json", "utf8");
-  //     })
-  //   )
-  // ).map((item) => JSON.parse(item));
-  return [];
 };
 
-async function main() {
-  console.log("running main");
-  getAllContractFiles();
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  // const Greeter = await hre.ethers.getContractFactory("Greeter");
-  // const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  // await greeter.deployed();
-
-  // console.log("Greeter deployed to:", greeter.address);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-// main()
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//     process.exit(1);
-//   });
-
-main();
+compileAndDeployAllContracts();
